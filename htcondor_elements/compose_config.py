f955824be51f00ed07f1a7b9d49ec5650da15eac
Carlo Emilio MONTANARI <carlidel95@gmail.com>
2022-10-31 17:26:36 +0100

    htcondor materials... hopefully

---
+++ htcondor_elements/compose_config.py
@@ -0,0 +1,29 @@
+import itertools
+
+mask_file_list = [
+    "/afs/cern.ch/work/c/public/lhc_dynamic_indicators/configs/masks1.json",
+    "/afs/cern.ch/work/c/public/lhc_dynamic_indicators/configs/masks2.json",
+    "/afs/cern.ch/work/c/public/lhc_dynamic_indicators/configs/masks3.json",
+]
+
+tracking_config_path = (
+    "/afs/cern.ch/work/c/public/lhc_dynamic_indicators/configs/tracking_htcondor.json"
+)
+
+zeta_list = ["min", "max", "avg"]
+
+tracking_options_list = [
+    "stability",
+    "log_displacement_singles",
+    "log_displacement_singles_birkhoff",
+    "megno_displacement",
+    "rem",
+    "tune_birkhoff",
+]
+
+
+for i, (mask, zeta, track) in enumerate(
+    itertools.product(mask_file_list, zeta_list, tracking_options_list)
+):
+    with open("configs/all_jobs_no_tune.txt", "a" if i > 0 else "w") as f:
+        f.write(f'"{mask}", "{tracking_config_path}", "{track}", "{zeta}"\n')
---
+++ htcondor_elements/job.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+export EOS_MGM_URL=root://eosuser.cern.ch
+export MYPYTHON=/afs/cern.ch/work/c/camontan/public/anaconda3
+
+unset PYTHONHOME
+unset PYTHONPATH
+source $MYPYTHON/bin/activate
+export PATH=$MYPYTHON/bin:$PATH
+
+which python
+
+# echo the 4 arguments received by the script
+echo $1
+echo $2
+echo $3
+echo $4
+
+python3 /afs/cern.ch/work/c/public/lhc_dynamic_indicators/run_sim.py --mask $1 --tracking $2 --kind $3 --output $3 --zeta $4
+
+eos cp *.h5 /eos/user/c/camontan/lhc_dynamic_data
+
+rm *.h5
